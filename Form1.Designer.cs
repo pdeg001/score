8c5435d6a084838f17bbdf5ee025fa9dd1893510
Petertje <pdeg@quicknet.nl>
2019-12-03 13:57:23 +0100

    Added images as carombole counter

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,47 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lbl_p1_1000 = new System.Windows.Forms.Label();
-            this.lbl_p1_100 = new System.Windows.Forms.Label();
+            this.pic_p1_1000 = new System.Windows.Forms.PictureBox();
             this.lbl_p1_10 = new System.Windows.Forms.Label();
             this.lbl_p1_1 = new System.Windows.Forms.Label();
             this.lbl_p2_1000 = new System.Windows.Forms.Label();
             this.lbl_p2_100 = new System.Windows.Forms.Label();
             this.lbl_p2_10 = new System.Windows.Forms.Label();
             this.lbl_p2_1 = new System.Windows.Forms.Label();
+            this.lbl_p1_100 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pic_p1_1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pic_p1_10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pic_p1_100 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_p1_1000)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_p1_1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_p1_10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_p1_100)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbl_p1_1000
             // 
-            this.lbl_p1_1000.Font = new System.Drawing.Font("Verdana", 225F);
-            this.lbl_p1_1000.Location = new System.Drawing.Point(25, 31);
+            this.lbl_p1_1000.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_p1_1000.Font = new System.Drawing.Font("Verdana", 200F);
+            this.lbl_p1_1000.ForeColor = System.Drawing.Color.Transparent;
+            this.lbl_p1_1000.Location = new System.Drawing.Point(236, 286);
             this.lbl_p1_1000.Margin = new System.Windows.Forms.Padding(10);
             this.lbl_p1_1000.Name = "lbl_p1_1000";
-            this.lbl_p1_1000.Size = new System.Drawing.Size(310, 357);
+            this.lbl_p1_1000.Size = new System.Drawing.Size(175, 208);
             this.lbl_p1_1000.TabIndex = 0;
             this.lbl_p1_1000.Tag = "1000";
             this.lbl_p1_1000.Text = "0";
             this.lbl_p1_1000.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.lbl_p1_1000.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
             // 
-            // lbl_p1_100
+            // pic_p1_1000
             // 
-            this.lbl_p1_100.Font = new System.Drawing.Font("Verdana", 225.25F);
-            this.lbl_p1_100.Location = new System.Drawing.Point(549, 31);
-            this.lbl_p1_100.Name = "lbl_p1_100";
-            this.lbl_p1_100.Size = new System.Drawing.Size(256, 306);
-            this.lbl_p1_100.TabIndex = 0;
-            this.lbl_p1_100.Tag = "100";
-            this.lbl_p1_100.Text = "0";
-            this.lbl_p1_100.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.lbl_p1_100.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            this.pic_p1_1000.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_p1_1000.Image = ((System.Drawing.Image)(resources.GetObject("pic_p1_1000.Image")));
+            this.pic_p1_1000.Location = new System.Drawing.Point(12, 62);
+            this.pic_p1_1000.Name = "pic_p1_1000";
+            this.pic_p1_1000.Size = new System.Drawing.Size(175, 208);
+            this.pic_p1_1000.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_p1_1000.TabIndex = 1;
+            this.pic_p1_1000.TabStop = false;
+            this.pic_p1_1000.Tag = "0";
+            this.pic_p1_1000.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownImgClick);
             // 
             // lbl_p1_10
             // 
-            this.lbl_p1_10.Font = new System.Drawing.Font("Verdana", 225.25F);
-            this.lbl_p1_10.Location = new System.Drawing.Point(287, 31);
+            this.lbl_p1_10.Font = new System.Drawing.Font("Verdana", 200.25F);
+            this.lbl_p1_10.Location = new System.Drawing.Point(748, 347);
             this.lbl_p1_10.Name = "lbl_p1_10";
-            this.lbl_p1_10.Size = new System.Drawing.Size(256, 306);
+            this.lbl_p1_10.Size = new System.Drawing.Size(294, 306);
             this.lbl_p1_10.TabIndex = 0;
             this.lbl_p1_10.Tag = "10";
             this.lbl_p1_10.Text = "0";
@@ -77,10 +97,10 @@
             // 
             // lbl_p1_1
             // 
-            this.lbl_p1_1.Font = new System.Drawing.Font("Verdana", 225F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_p1_1.Location = new System.Drawing.Point(811, 31);
+            this.lbl_p1_1.Font = new System.Drawing.Font("Verdana", 200.25F);
+            this.lbl_p1_1.Location = new System.Drawing.Point(1015, 347);
             this.lbl_p1_1.Name = "lbl_p1_1";
-            this.lbl_p1_1.Size = new System.Drawing.Size(256, 306);
+            this.lbl_p1_1.Size = new System.Drawing.Size(294, 306);
             this.lbl_p1_1.TabIndex = 0;
             this.lbl_p1_1.Tag = "1";
             this.lbl_p1_1.Text = "0";
@@ -90,7 +110,7 @@
             // lbl_p2_1000
             // 
             this.lbl_p2_1000.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_p2_1000.Location = new System.Drawing.Point(1486, 31);
+            this.lbl_p2_1000.Location = new System.Drawing.Point(1527, 428);
             this.lbl_p2_1000.Name = "lbl_p2_1000";
             this.lbl_p2_1000.Size = new System.Drawing.Size(91, 104);
             this.lbl_p2_1000.TabIndex = 0;
@@ -101,7 +121,7 @@
             // lbl_p2_100
             // 
             this.lbl_p2_100.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_p2_100.Location = new System.Drawing.Point(1583, 31);
+            this.lbl_p2_100.Location = new System.Drawing.Point(1624, 428);
             this.lbl_p2_100.Name = "lbl_p2_100";
             this.lbl_p2_100.Size = new System.Drawing.Size(91, 104);
             this.lbl_p2_100.TabIndex = 0;
@@ -112,7 +132,7 @@
             // lbl_p2_10
             // 
             this.lbl_p2_10.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_p2_10.Location = new System.Drawing.Point(1680, 31);
+            this.lbl_p2_10.Location = new System.Drawing.Point(1721, 428);
             this.lbl_p2_10.Name = "lbl_p2_10";
             this.lbl_p2_10.Size = new System.Drawing.Size(91, 104);
             this.lbl_p2_10.TabIndex = 0;
@@ -123,7 +143,7 @@
             // lbl_p2_1
             // 
             this.lbl_p2_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_p2_1.Location = new System.Drawing.Point(1777, 31);
+            this.lbl_p2_1.Location = new System.Drawing.Point(1818, 428);
             this.lbl_p2_1.Name = "lbl_p2_1";
             this.lbl_p2_1.Size = new System.Drawing.Size(91, 104);
             this.lbl_p2_1.TabIndex = 0;
@@ -131,11 +151,120 @@
             this.lbl_p2_1.Text = "0";
             this.lbl_p2_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
             // 
+            // lbl_p1_100
+            // 
+            this.lbl_p1_100.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_p1_100.Font = new System.Drawing.Font("Verdana", 200F);
+            this.lbl_p1_100.Location = new System.Drawing.Point(525, 347);
+            this.lbl_p1_100.Margin = new System.Windows.Forms.Padding(3, 0, 3, 25);
+            this.lbl_p1_100.Name = "lbl_p1_100";
+            this.lbl_p1_100.Size = new System.Drawing.Size(294, 306);
+            this.lbl_p1_100.TabIndex = 0;
+            this.lbl_p1_100.Tag = "100";
+            this.lbl_p1_100.Text = "0";
+            this.lbl_p1_100.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbl_p1_100.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(1714, 62);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(175, 208);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pic_p1_1
+            // 
+            this.pic_p1_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_p1_1.Image = global::peter.Properties.Resources._0;
+            this.pic_p1_1.Location = new System.Drawing.Point(549, 62);
+            this.pic_p1_1.Name = "pic_p1_1";
+            this.pic_p1_1.Size = new System.Drawing.Size(175, 208);
+            this.pic_p1_1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_p1_1.TabIndex = 1;
+            this.pic_p1_1.TabStop = false;
+            this.pic_p1_1.Tag = "0";
+            this.pic_p1_1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownImgClick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(1535, 62);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(175, 208);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pic_p1_10
+            // 
+            this.pic_p1_10.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_p1_10.Image = ((System.Drawing.Image)(resources.GetObject("pic_p1_10.Image")));
+            this.pic_p1_10.Location = new System.Drawing.Point(370, 62);
+            this.pic_p1_10.Name = "pic_p1_10";
+            this.pic_p1_10.Size = new System.Drawing.Size(175, 208);
+            this.pic_p1_10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_p1_10.TabIndex = 1;
+            this.pic_p1_10.TabStop = false;
+            this.pic_p1_10.Tag = "0";
+            this.pic_p1_10.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownImgClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(1356, 62);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(175, 208);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pic_p1_100
+            // 
+            this.pic_p1_100.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pic_p1_100.Image = ((System.Drawing.Image)(resources.GetObject("pic_p1_100.Image")));
+            this.pic_p1_100.Location = new System.Drawing.Point(191, 62);
+            this.pic_p1_100.Name = "pic_p1_100";
+            this.pic_p1_100.Size = new System.Drawing.Size(175, 208);
+            this.pic_p1_100.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_p1_100.TabIndex = 1;
+            this.pic_p1_100.TabStop = false;
+            this.pic_p1_100.Tag = "0";
+            this.pic_p1_100.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownImgClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1177, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 208);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1904, 450);
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(1904, 961);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pic_p1_1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pic_p1_10);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pic_p1_100);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pic_p1_1000);
             this.Controls.Add(this.lbl_p2_1);
             this.Controls.Add(this.lbl_p2_10);
             this.Controls.Add(this.lbl_p2_100);
@@ -144,9 +273,18 @@
             this.Controls.Add(this.lbl_p1_10);
             this.Controls.Add(this.lbl_p1_100);
             this.Controls.Add(this.lbl_p1_1000);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Main";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_p1_1000)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_p1_1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_p1_10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_p1_100)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -161,6 +299,14 @@
         private System.Windows.Forms.Label lbl_p2_100;
         private System.Windows.Forms.Label lbl_p2_10;
         private System.Windows.Forms.Label lbl_p2_1;
+        private System.Windows.Forms.PictureBox pic_p1_1000;
+        private System.Windows.Forms.PictureBox pic_p1_100;
+        private System.Windows.Forms.PictureBox pic_p1_10;
+        private System.Windows.Forms.PictureBox pic_p1_1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace peter
 {
     public partial class Form1 : Form
     {
+        public string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -33,6 +36,7 @@ namespace peter
             int points = 0;
             int pointsPassed = 0;
             int carom = 0;
+            double test = 0000;
             string newCarom;
             string oldCarom;
             string labelPoints;
@@ -57,7 +61,6 @@ namespace peter
             oldCarom = lbl1000.Text + lbl100.Text + lbl10.Text + lbl1.Text;
             carom = Int32.Parse(oldCarom);
 
-
             if (e.Button == MouseButtons.Left)
             {
                 carom += pointsPassed;
@@ -76,18 +79,85 @@ namespace peter
                 }
 
             }
+           
+            Console.WriteLine(carom);
 
             PadStr padstr = new PadStr();
             padstr.carom = carom;
-
             newCarom = padstr.GenPad();
-
             points = lblVal + carom;
             lbl1.Text = newCarom.Substring(3, 1);
             lbl10.Text = newCarom.Substring(2, 1);
             lbl100.Text = newCarom.Substring(1, 1);
             lbl1000.Text = newCarom.Substring(0, 1);
-        
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            string fileName = lbl.Text+".png";
+            string path = Path.Combine(projectDirectory, @"images\", fileName);
+            Image img = Image.FromFile(path);
+         //   pic_p1_1000.Image = img; // Image.FromFile(path);
+            PictureBox pic;
+            pic = Controls.Find("pic_" + playerNumber + "_" + pointsPassed, true).FirstOrDefault() as PictureBox;
+            pic.Image = img;
+            pic.Tag = lbl.Text;
+            //pic_p1_1
+
+        }
+
+
+        private void Form1_MouseDownImgClick(object sender, MouseEventArgs e)
+        {
+            int carom = 0;
+            string padCarom = "";
+
+            PictureBox pic = sender as PictureBox;
+            string picName = pic.Name;
+            string[] picNameSplit = picName.Split('_');
+            string player = picNameSplit.GetValue(1).ToString();
+            int passedValue = Int32.Parse(picNameSplit.GetValue(2).ToString());
+
+            PictureBox pic1000 = Controls.Find("pic_" + player + "_1000", true).FirstOrDefault() as PictureBox;
+            PictureBox pic100 = Controls.Find("pic_" + player + "_100", true).FirstOrDefault() as PictureBox;
+            PictureBox pic10 = Controls.Find("pic_" + player + "_10", true).FirstOrDefault() as PictureBox;
+            PictureBox pic1 = Controls.Find("pic_" + player + "_1", true).FirstOrDefault() as PictureBox;
+
+            string p1Carom = pic1000.Tag.ToString() + pic100.Tag.ToString() + pic10.Tag.ToString() + pic1.Tag.ToString();
+            carom = Int32.Parse(p1Carom);
+            if (e.Button == MouseButtons.Left)
+            {
+                carom += passedValue;
+
+                if (carom > 9999)
+                {
+                    return;
+                }
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                carom -= passedValue;
+                if (carom <= -1)
+                {
+                    return;
+                }
+
+            }
+
+            PadStr padstr = new PadStr();
+            padstr.carom = carom;
+            string newCarom = padstr.GenPad();
+
+            pic1000.Tag = newCarom.Substring(0, 1);
+            pic100.Tag = newCarom.Substring(1, 1);
+            pic10.Tag = newCarom.Substring(2, 1);
+            pic1.Tag = newCarom.Substring(3, 1);
+            GetNumPic numpic = new GetNumPic();
+            numpic.imgName = pic1.Tag.ToString() + ".png";
+            pic1.Image = numpic.getPic();
+            numpic.imgName = pic10.Tag.ToString() + ".png";
+            pic10.Image = numpic.getPic();
+            numpic.imgName = pic100.Tag.ToString() + ".png";
+            pic100.Image = numpic.getPic();
+            numpic.imgName = pic1000.Tag.ToString() + ".png";
+            pic1000.Image = numpic.getPic();
         }
 
         class PadStr
@@ -98,6 +168,19 @@ namespace peter
                 return carom.ToString().PadLeft(4, '0');
             }
 
+        }
+
+
+        class GetNumPic
+        {
+            public string imgName = "";
+            public Image img; 
+            string projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            public Image getPic()
+            {
+                
+                return Image.FromFile(Path.Combine(projectDirectory, @"images\", imgName));
+            }
         }
     }
 }
