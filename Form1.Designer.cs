d08445ad7278fe845228c2292b5c007e95b8af85
Petertje <pdeg@quicknet.nl>
2019-12-03 06:26:21 +0100

    file commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace peter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_p1_1000 = new System.Windows.Forms.Label();
+            this.lbl_p1_100 = new System.Windows.Forms.Label();
+            this.lbl_p1_10 = new System.Windows.Forms.Label();
+            this.lbl_p1_1 = new System.Windows.Forms.Label();
+            this.lbl_p2_1000 = new System.Windows.Forms.Label();
+            this.lbl_p2_100 = new System.Windows.Forms.Label();
+            this.lbl_p2_10 = new System.Windows.Forms.Label();
+            this.lbl_p2_1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_p1_1000
+            // 
+            this.lbl_p1_1000.Font = new System.Drawing.Font("Verdana", 225F);
+            this.lbl_p1_1000.Location = new System.Drawing.Point(25, 31);
+            this.lbl_p1_1000.Margin = new System.Windows.Forms.Padding(10);
+            this.lbl_p1_1000.Name = "lbl_p1_1000";
+            this.lbl_p1_1000.Size = new System.Drawing.Size(310, 357);
+            this.lbl_p1_1000.TabIndex = 0;
+            this.lbl_p1_1000.Tag = "1000";
+            this.lbl_p1_1000.Text = "0";
+            this.lbl_p1_1000.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_p1_1000.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            // 
+            // lbl_p1_100
+            // 
+            this.lbl_p1_100.Font = new System.Drawing.Font("Verdana", 225.25F);
+            this.lbl_p1_100.Location = new System.Drawing.Point(549, 31);
+            this.lbl_p1_100.Name = "lbl_p1_100";
+            this.lbl_p1_100.Size = new System.Drawing.Size(256, 306);
+            this.lbl_p1_100.TabIndex = 0;
+            this.lbl_p1_100.Tag = "100";
+            this.lbl_p1_100.Text = "0";
+            this.lbl_p1_100.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbl_p1_100.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            // 
+            // lbl_p1_10
+            // 
+            this.lbl_p1_10.Font = new System.Drawing.Font("Verdana", 225.25F);
+            this.lbl_p1_10.Location = new System.Drawing.Point(287, 31);
+            this.lbl_p1_10.Name = "lbl_p1_10";
+            this.lbl_p1_10.Size = new System.Drawing.Size(256, 306);
+            this.lbl_p1_10.TabIndex = 0;
+            this.lbl_p1_10.Tag = "10";
+            this.lbl_p1_10.Text = "0";
+            this.lbl_p1_10.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbl_p1_10.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            // 
+            // lbl_p1_1
+            // 
+            this.lbl_p1_1.Font = new System.Drawing.Font("Verdana", 225F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_p1_1.Location = new System.Drawing.Point(811, 31);
+            this.lbl_p1_1.Name = "lbl_p1_1";
+            this.lbl_p1_1.Size = new System.Drawing.Size(256, 306);
+            this.lbl_p1_1.TabIndex = 0;
+            this.lbl_p1_1.Tag = "1";
+            this.lbl_p1_1.Text = "0";
+            this.lbl_p1_1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbl_p1_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            // 
+            // lbl_p2_1000
+            // 
+            this.lbl_p2_1000.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_p2_1000.Location = new System.Drawing.Point(1486, 31);
+            this.lbl_p2_1000.Name = "lbl_p2_1000";
+            this.lbl_p2_1000.Size = new System.Drawing.Size(91, 104);
+            this.lbl_p2_1000.TabIndex = 0;
+            this.lbl_p2_1000.Tag = "1000";
+            this.lbl_p2_1000.Text = "0";
+            this.lbl_p2_1000.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            // 
+            // lbl_p2_100
+            // 
+            this.lbl_p2_100.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_p2_100.Location = new System.Drawing.Point(1583, 31);
+            this.lbl_p2_100.Name = "lbl_p2_100";
+            this.lbl_p2_100.Size = new System.Drawing.Size(91, 104);
+            this.lbl_p2_100.TabIndex = 0;
+            this.lbl_p2_100.Tag = "100";
+            this.lbl_p2_100.Text = "0";
+            this.lbl_p2_100.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            // 
+            // lbl_p2_10
+            // 
+            this.lbl_p2_10.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_p2_10.Location = new System.Drawing.Point(1680, 31);
+            this.lbl_p2_10.Name = "lbl_p2_10";
+            this.lbl_p2_10.Size = new System.Drawing.Size(91, 104);
+            this.lbl_p2_10.TabIndex = 0;
+            this.lbl_p2_10.Tag = "10";
+            this.lbl_p2_10.Text = "0";
+            this.lbl_p2_10.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            // 
+            // lbl_p2_1
+            // 
+            this.lbl_p2_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_p2_1.Location = new System.Drawing.Point(1777, 31);
+            this.lbl_p2_1.Name = "lbl_p2_1";
+            this.lbl_p2_1.Size = new System.Drawing.Size(91, 104);
+            this.lbl_p2_1.TabIndex = 0;
+            this.lbl_p2_1.Tag = "1";
+            this.lbl_p2_1.Text = "0";
+            this.lbl_p2_1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDownCp1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1904, 450);
+            this.Controls.Add(this.lbl_p2_1);
+            this.Controls.Add(this.lbl_p2_10);
+            this.Controls.Add(this.lbl_p2_100);
+            this.Controls.Add(this.lbl_p1_1);
+            this.Controls.Add(this.lbl_p2_1000);
+            this.Controls.Add(this.lbl_p1_10);
+            this.Controls.Add(this.lbl_p1_100);
+            this.Controls.Add(this.lbl_p1_1000);
+            this.Name = "Form1";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_p1_1000;
+        private System.Windows.Forms.Label lbl_p1_100;
+        private System.Windows.Forms.Label lbl_p1_10;
+        private System.Windows.Forms.Label lbl_p1_1;
+        private System.Windows.Forms.Label lbl_p2_1000;
+        private System.Windows.Forms.Label lbl_p2_100;
+        private System.Windows.Forms.Label lbl_p2_10;
+        private System.Windows.Forms.Label lbl_p2_1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace peter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // private void Form1_MouseDown(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void Form1_MouseDownCp1(object sender, MouseEventArgs e)
+        {
+            int lblVal = 0;
+            int points = 0;
+            int pointsPassed = 0;
+            int carom = 0;
+            string newCarom;
+            string oldCarom;
+            string labelPoints;
+            string playerNumber;
+            Label lbl1000, lbl100, lbl10, lbl1;
+
+            Label lbl = sender as Label;
+            object tag = lbl.Tag;
+
+
+            string[] labelValue = lbl.Name.Split('_');
+            pointsPassed = Int32.Parse(labelValue.GetValue(labelValue.Length-1).ToString());
+            playerNumber = labelValue.GetValue(1).ToString();
+
+            lbl1000 = Controls.Find("lbl_"+playerNumber+"_1000", true).FirstOrDefault() as Label;
+            lbl100 = Controls.Find("lbl_"+playerNumber+"_100", true).FirstOrDefault() as Label;
+            lbl10 = Controls.Find("lbl_"+playerNumber+"_10", true).FirstOrDefault() as Label;
+            lbl1 = Controls.Find("lbl_"+playerNumber+"_1", true).FirstOrDefault() as Label;
+          
+            lblVal = Int32.Parse(lbl.Text);
+            pointsPassed = Convert.ToInt32(lbl.Tag);
+            oldCarom = lbl1000.Text + lbl100.Text + lbl10.Text + lbl1.Text;
+            carom = Int32.Parse(oldCarom);
+
+
+            if (e.Button == MouseButtons.Left)
+            {
+                carom += pointsPassed;
+
+                if (carom > 9999)
+                {
+                    return;
+                }
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+                carom -= pointsPassed;
+                if (carom <= -1)
+                {
+                    return;
+                }
+
+            }
+
+            PadStr padstr = new PadStr();
+            padstr.carom = carom;
+
+            newCarom = padstr.GenPad();
+
+            points = lblVal + carom;
+            lbl1.Text = newCarom.Substring(3, 1);
+            lbl10.Text = newCarom.Substring(2, 1);
+            lbl100.Text = newCarom.Substring(1, 1);
+            lbl1000.Text = newCarom.Substring(0, 1);
+        
+        }
+
+        class PadStr
+        {
+            public int carom = 0;
+                 public string GenPad()
+            {
+                return carom.ToString().PadLeft(4, '0');
+            }
+
+        }
+    }
+}
